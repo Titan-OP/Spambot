b2d4a7375d603648aad999f443b75b67be166a25
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-07-28 09:59:18 +0530

    Update spam.py

--- spambot/modules/spam.py
+++ spambot/modules/spam.py
@@ -23,28 +23,28 @@ async def gladiators(event):
   if event.sender_id in SUDO_USERS or event.sender_id in DEV_USERS:
     try:
       xD = str(event.text[6:])
-      ldr = xD.split(";")
+      ldr = xD.split(" ", 1)
       counter = int(ldr[0])
       mesg = ldr[1]
-      if counter > 2000:
-        return await event.reply("Please give value under 2000")
+      if counter > 99:
+        return await event.reply("Please give value greater than or equal to 99.")
       for i in range(counter):
         await event.client.send_message(event.chat, mesg)
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
       await event.client.send_message(event.chat, "Spammed successfully!!\n© @TeamGladiators")
     except Exception as xy:
       await event.reply("Oops!! Something went wrong, forward this message to @Gladiators_Support\n\n" + str(xy))
+
+      
 @register(pattern="^/dspam(?: |$)(.*)")
 async def gladiators(event):
   if event.sender_id in SUDO_USERS or event.sender_id in DEV_USERS:
     try:
       xD = str(event.text[7:])
-      lst = xD.split(";")
+      lst = xD.split(" ", 2)
       tme = float(lst[0])
       counter = int(lst[1])
       mesg = str(lst[2])
-      if counter > 2000:
-        return await event.reply("Please give value under 2000")
       for i in range(counter):
         await event.client.send_message(event.chat, mesg)
         await asyncio.sleep(tme)
@@ -61,8 +61,8 @@ async def gladiators(e):
         text = e.text.split()
         counter = int(text[1])
         counter += 1
-        if counter > 2000:
-          return await e.reply("Please give value under 2000")
+        if counter > 99:
+          return await e.reply("Please give value greater than or equal to 99.")
         media = await reply.download_media( "SpamBot/downloads/")
         for i in range(1, counter):
           await e.client.send_file(e.chat_id, media)
@@ -78,7 +78,7 @@ async def _(e):
       try:
         x = await e.get_reply_message()
         if not (x and x.media and hasattr(x.media, "document")):
-          return await e.reply("`Reply To Sticker Only`")
+          return await e.reply("`Reply To Sticker Only.`")
         set = x.document.attributes[1]
         sset = await e.client(
           GetStickerSetRequest(
@@ -101,3 +101,22 @@ async def _(e):
         await e.client.send_message(e.chat, "Spammed successfully!!\n© @TeamGladiators")
       except Exception as xy:
         await e.reply("Oops!! Something went wrong, forward this message to @Gladiators_Support\n\n" + str(xy))
+        
+        
+        
+@register(pattern="^/bigspam(?: |$)(.*)")
+async def gladiators(event):
+  if event.sender_id in SUDO_USERS or event.sender_id in DEV_USERS:
+    try:
+      xD = str(event.text[6:])
+      ldr = xD.split(" ", 1)
+      counter = int(ldr[0])
+      mesg = ldr[1]
+      if counter > 2000:
+        return await event.reply("`Please give value greater than or equal to 2000.`")
+      for i in range(counter):
+        await event.client.send_message(event.chat, mesg)
+        await asyncio.sleep(2)
+      await event.client.send_message(event.chat, "Spammed successfully!!\n© @TeamGladiators")
+    except Exception as xy:
+      await event.reply("Oops!! Something went wrong, forward this message to @Gladiators_Support\n\n" + str(xy))
