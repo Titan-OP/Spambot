51ed4f301ec8232813d2fe3d9eb81dd6333db131
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-08-03 22:02:52 +0530

    Update telethonbasics.py

--- spambot/pyrogramee/telethonbasics.py
+++ spambot/pyrogramee/telethonbasics.py
@@ -125,33 +125,6 @@ async def is_admin(event, user):
     return is_mod
 
 
-async def progress(current, total, event, start, type_of_ps, file_name=None):
-    """Generic progress_callback for both
-    upload.py and download.py"""
-    now = time.time()
-    diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "[{0}{1}]\nProgress: {2}%\n".format(
-            "".join(["🟠" for i in range(math.floor(percentage / 5))]),
-            "".join(["🔘" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2),
-        )
-        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
-            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
-        )
-        if file_name:
-            await event.edit(
-                "{}\nFile Name: `{}`\n{}".format(type_of_ps, file_name, tmp)
-            )
-        else:
-            await event.edit("{}\n{}".format(type_of_ps, tmp))
-
-
 def humanbytes(size):
     """Input size in bytes,
     outputs in a human readable format"""
