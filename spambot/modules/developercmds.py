64342c943670e113f7524e192eba6655da095530
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-08-08 16:41:22 +0530

    Update developercmds.py

--- spambot/modules/developercmds.py
+++ spambot/modules/developercmds.py
@@ -28,7 +28,25 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Upda
 
 DEFAULTUSER = str(MASTER_NAME)
 
-
+def TeamArsenic_time(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    TeamArsenic_ret = (
+        ((str(weeks) + "ᴡ:") if weeks else "")
+        + ((str(days) + "ᴅ:") if days else "")
+        + ((str(hours) + "ʜ:") if hours else "")
+        + ((str(minutes) + "ᴍ:") if minutes else "")
+        + ((str(seconds) + "s:") if seconds else "")
+    )
+    if TeamArsenic_ret.endswith(":"):
+        return TeamArsenic_ret[:-1]
+    else:
+        return TeamArsenic_ret
 
 
 ping_buttons = [
@@ -51,7 +69,7 @@ def ping(update: Update, context: CallbackContext):
     ping_start = datetime.now()
     ping_end = datetime.now()
     ms = (ping_end-ping_start).microseconds / 1000
-    uptime = time_formatter((time.time() - Start_time) * 1000)
+    uptime = TeamArsenic_time((time.time() - Start_time) * 1000)
     update.effective_message.reply_text(f"**•• Pᴏɴɢ !! ••**\n**⏱ Pɪɴɢ sᴘᴇᴇᴅ : {ms}ᴍs**\n**⏳ Uᴘᴛɪᴍᴇ - {uptime}**")
 
 
