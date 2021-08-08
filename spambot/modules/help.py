b0e8e3b6e625d39f6a60162f277a312cb9bd5644
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-08-08 16:42:12 +0530

    Update help.py

--- spambot/modules/help.py
+++ spambot/modules/help.py
@@ -35,6 +35,25 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Upda
 
 
 
+def TeamArsenic_time(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    TeamArsenic_ret = (
+        ((str(weeks) + "ᴡ:") if weeks else "")
+        + ((str(days) + "ᴅ:") if days else "")
+        + ((str(hours) + "ʜ:") if hours else "")
+        + ((str(minutes) + "ᴍ:") if minutes else "")
+        + ((str(seconds) + "s:") if seconds else "")
+    )
+    if TeamArsenic_ret.endswith(":"):
+        return TeamArsenic_ret[:-1]
+    else:
+        return TeamArsenic_ret
 
 DEFAULTUSER = str(MASTER_NAME)
 help_img = "https://telegra.ph/file/6e92103071aa47ee7023e.mp4"
@@ -173,7 +192,7 @@ def help_menu(update, context):
         ping_start = datetime.now()
         ping_end = datetime.now()
         ms = (ping_end-ping_start).microseconds / 1000
-        uptime = time_formatter((time.time() - Start_time) * 1000)
+        uptime = TeamArsenic_time((time.time() - Start_time) * 1000)
         pong = f"""
         **•• Pᴏɴɢ !! ••**
         **⏱ Pɪɴɢ sᴘᴇᴇᴅ : {ms}ᴍs**
