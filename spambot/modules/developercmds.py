f7b716bf11ec9cfeceb5f12c5d2ac132d5fda998
Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>
2021-08-18 06:38:31 +0530

    Update developercmds.py

--- spambot/modules/developercmds.py
+++ spambot/modules/developercmds.py
@@ -93,46 +93,46 @@ def leave(update: Update, context: CallbackContext):
         update.effective_message.reply_text("**Am I a joke to you?\nSend a valid chat id bruhh!**")
 
 
-@run_async
-@sudo_plus
-def renovate(update: Update, context: CallbackContext):
-    message = update.effective_message
-    user = update.effective_user
-    chat = update.effective_chat
-    bot, args = context.bot, context.args
+# @run_async
+# @sudo_plus
+# def renovate(update: Update, context: CallbackContext):
+#     message = update.effective_message
+#     user = update.effective_user
+#     chat = update.effective_chat
+#     bot, args = context.bot, context.args
     
     
     
-    sent_msg = update.effective_message.reply_text(
-        "Trying to sync with github repo..."
-    )
-    subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
+#     sent_msg = update.effective_message.reply_text(
+#         "Trying to sync with github repo..."
+#     )
+#     subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
 
-    sent_msg_text = sent_msg.text + "\n\nI guess bot is synced... restarting in "
+#     sent_msg_text = sent_msg.text + "\n\nI guess bot is synced... restarting in "
 
-    for i in reversed(range(5)):
-        sent_msg.edit_text(sent_msg_text + str(i + 1))
-        sleep(1)
+#     for i in reversed(range(5)):
+#         sent_msg.edit_text(sent_msg_text + str(i + 1))
+#         sleep(1)
 
-    sent_msg.edit_text("Restarted.")
+#     sent_msg.edit_text("Restarted.")
 
-    os.system("restart.bat")
-    os.execv("start.bat", sys.argv)
+#     os.system("restart.bat")
+#     os.execv("start.bat", sys.argv)
 
 
 
 
 
 LEAVE_HANDLER = CommandHandler("leave", leave)
-UPDATE_HANDLER = CommandHandler("updates", renovate)
+# UPDATE_HANDLER = CommandHandler("updates", renovate)
 RESTART_HANDLER = CommandHandler("restart", restart)
 PING_HANDLER = CommandHandler("ping", ping)
 
 
 dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(PING_HANDLER)
-dispatcher.add_handler(UPDATE_HANDLER)
+# dispatcher.add_handler(UPDATE_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
 __mod_name__ = "developercmds"
-__handlers__ = [UPDATE_HANDLER, RESTART_HANDLER, LEAVE_HANDLER, PING_HANDLER]
+__handlers__ = [RESTART_HANDLER, LEAVE_HANDLER, PING_HANDLER]
