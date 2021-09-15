2bca4362ddf55cdda3318eecda23396984140ce8
тєcнησ✘ρяσ <83203983+Titan-OP@users.noreply.github.com>
2021-09-15 19:14:01 +0530

    Update __main__.py

--- spambot/__main__.py
+++ spambot/__main__.py
@@ -343,7 +343,7 @@ def main():
     dispatcher.add_error_handler(error_callback)
 
     if WEBHOOK:
-        LOGGER.info("Starting gladiators spambot using webhooks.")
+        LOGGER.info("STARTED TOXIC SPAMBOT SUCCESSFULLY..!!\n Go and Check By Using /ping .")
         updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 
         if CERT_PATH:
@@ -352,7 +352,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Starting gladiators spambot...")
+        LOGGER.info("STARTED TOXIC SPAMBOT SUCCESSFULLY..!!\n Go and Check By Using /ping .")
         updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
